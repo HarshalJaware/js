1a4fc8cbeb4d51f6590fca165c9bc2b7f3161c44
HarshalJaware <jawareharshal93@gmail.com>
2025-02-13 10:51:59 +0000

    operations

---
+++ 01/operations.js
@@ -0,0 +1,29 @@
+let val = 3;
+let negVal = -val;
+
+console.log(negVal)
+
+console.log(2 + 2)
+console.log(3 - 2)
+console.log(2 * 2)
+console.log(5 / 2)
+console.log(2 ** 2)
+console.log(2%3)
+
+
+
+console.log(+true) //1
+console.log(+"") // 0
+
+
+
+let x = 3;
+const y = ++x;
+
+//console.log(`x:${x},y:${y}`)
+
+
+let x2 = 3n;
+const y2 = x2++;
+
+console.log(`x2:${x2},y2:${y2}`)
