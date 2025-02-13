f938b35ecd0e0982f1adfd475607d573e5fb6771
HarshalJaware <jawareharshal93@gmail.com>
2025-02-13 10:13:07 +0000

    conversion of data types

---
+++ 01/datatypes_conversion.js
@@ -0,0 +1,25 @@
+let score = true;
+let newVal = Number(score);
+//console.log(typeof newVal);
+//console.log(newVal)
+
+
+ 
+//"33" ==>number
+//"33abc"  ==>typeof number but NAN
+// null ==>0 Number
+//undefined  ==> number NAN
+
+
+
+let isDraft =null;
+
+let newDraftVal = Boolean(isDraft);
+console.log(typeof newDraftVal);
+console.log(newDraftVal)
+
+
+//1 =>true 0=>false
+// 'test' =>true
+
+
