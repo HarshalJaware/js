98df9d5b7e9d724d1a7fb909a11df53d14861023
HarshalJaware <jawareharshal93@gmail.com>
2025-02-13 11:29:54 +0000

    datatypes summary

---
+++ 01/datatypes_summary.js
@@ -0,0 +1,30 @@
+//javascript is dynamically typed
+//Two Types of data types:-
+//1.Primitive Data Types
+//2.Non-Primitive Data Types
+
+
+//1.Primitive Data Types
+//String
+//Number
+//Boolean
+//NUll
+//Undefined
+//BigInt
+//Symbol
+
+
+
+//2.Non-Primitive Data Types
+//Refernce
+//Array
+//Objects
+//Functions
+
+
+
+let a = Symbol("123");
+let b = Symbol("123");
+console.log(a === b);
+console.log(a);
+console.log(b);
