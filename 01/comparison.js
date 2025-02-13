a2b63d791954f16769b14792041b42f7fdbd46fd
HarshalJaware <jawareharshal93@gmail.com>
2025-02-13 11:06:11 +0000

    comparison

---
+++ 01/comparison.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 === 1);
+// console.log(2 == 2);
+// console.log(2 == "2");
+// console.log(2 === "2");
+// console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log(1 < "2");
+console.log(null == undefined)
+console.log(null < undefined)
+console.log(null > undefined)
+
+
+
