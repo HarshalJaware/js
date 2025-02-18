0ce2c3199636a1dde07caff240b4512d267f877b
HarshalJaware <jawareharshal93@gmail.com>
2025-02-18 07:10:38 +0000

    numbers and maths

---
+++ 01/number_and_maths.js
@@ -0,0 +1,29 @@
+// const total = 400;
+// console.log(total);
+// console.log(typeof total);
+
+// const balance = new Number(5000000);
+// console.log(balance)
+// console.log(balance.toExponential())
+// console.log(balance.toFixed(2))
+// console.log(balance.toLocaleString())
+
+
+
+/************************Maths*************************************/
+console.log(Math.abs(-4)); //expected op: 4
+console.log(Math.round(4.2)); //expected op: 4
+console.log(Math.round(4.9)); //expected op: 5
+
+console.log(Math.ceil(4.2)); //expected op: 5
+console.log(Math.floor(4.2)); //expected op: 4
+
+console.log(Math.pow(3,2)); //expected op: 9
+console.log(Math.min(1,2,3,4,5,6)); //expected op: 1
+
+console.log(Math.max(1,2,3,4,5,6)); //expected op: 6
+
+
+
+
+
